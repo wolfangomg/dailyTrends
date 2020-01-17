bf2e1f3d7ef3b3e4a02cb0f7e5cbdc227acab671
wolfangomg <seyzer92@gmail.com>
2020-01-17 11:27:03 +0100

    3. Crear un controlador CRUD

--- app/src/app/feed/default/default.component.ts
+++ app/src/app/feed/default/default.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Feed } from 'src/app/model/feed';
 
 @Component({
   selector: 'app-feed',
@@ -7,21 +9,56 @@ import { Router } from '@angular/router';
   styleUrls: ['./default.component.scss']
 })
 export class DefaultComponent implements OnInit {
-  @Input() id: number;
-  @Input() title: string;
-  @Input() body: string;
-  @Input() source: string;
-  @Input() publisher: string;
-  @Input() image: string;
+  @Input() config: Feed;
 
+  @Output() onupdate: EventEmitter<any> = new EventEmitter();
+  public form: FormGroup;
+  public loading: boolean;
+  public editMode: boolean;
   constructor(private router: Router) { }
 
+  private defineForm(): void {
+    this.form = new FormGroup({
+      title: new FormControl({ value: this.config.title, disabled: false }, [Validators.required]),
+      body: new FormControl({ value: this.config.body, disabled: false }, [Validators.required]),
+      source: new FormControl({ value: this.config.source, disabled: false }),
+      publisher: new FormControl({ value: this.config.publisher, disabled: false }),
+      image: new FormControl({ value: this.config.image, disabled: false })
+    });
+  }
+
   ngOnInit() {
+    this.defineForm();
+  }
+
+  public clickEdit(): void {
+    this.editMode = true;
+  }
+
+  public clickDelete(): void {
+    this.onupdate.emit({
+      config: this.config,
+      action: 'delete'
+    });
+  }
+
+  public clickCancel(): void {
+    this.editMode = false;
+  }
+
+  public onSubmit(): void {
+    this.loading = true;
+    this.editMode = false;
+    this.config.title = this.form.value.title;
+    this.config.body = this.form.value.body;
+    this.config.source = this.form.value.source;
+    this.config.publisher = this.form.value.publisher;
+    this.config.image = this.form.value.image;
+    setTimeout(() => {
+      this.loading = false;
+    }, 800);
 
   }
 
-  public clickDetails(): void {
-    this.router.navigateByUrl('/' + this.id);
-  }
 
 }
